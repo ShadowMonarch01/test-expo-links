95f722246924f73f55b404268795c0290a5d9d11
ShadowMonarch01 <gunngptrust@gmail.com>
2024-11-11 15:52:42 +0100

    links added

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
     <div className='container'>
       <h1>Deep Linking Test Page</h1>
       <p>
-        <a href="https://yourapp.vercel.app/home">Open Home Screen in App</a>
+        <a href="https://test-expo-links.vercel.app/home">Open Home Screen in App</a>
       </p>
       <p>
-        <a href="https://yourapp.vercel.app/details/1">Open Details Screen with ID 1</a>
+        <a href="https://test-expo-links.vercel.app/details/1">Open Details Screen with ID 1</a>
       </p>
     </div>
   )
